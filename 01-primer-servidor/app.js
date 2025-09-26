8c4afbfa0001e79ad04c5fe02c8d5b0daa864e9c
jfmartinez10 <jfmartinez383@gmail.com>
2025-09-26 12:44:05 +0200

    Primer servidor funcional!

---
+++ 01-primer-servidor/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+
+const PORT = 80;
+
+const app = express();
+
+//Modo de uso de JSON
+app.use(express.json()); 
+
+//Endpoint raiz
+app.get("/", (req, res) => { //request y response
+    //Enviamos desde tipo json
+    res.send({
+        mensaje: "Hola mundo"
+    })
+})
+
+//Escuchar en el puerto 3000
+app.listen(PORT, () => {
+    console.log("Servidor escuchando el puerto " + PORT);
+})
+
