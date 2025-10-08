21cc24e7e7cd68a9f23ca7f05a45d7e5e4052e14
jfmartinez10 <jfmartinez383@gmail.com>
2025-10-08 13:06:16 +0200

    Feat: simulador db

---
+++ 02-estructura/db/memory.js
@@ -0,0 +1,33 @@
+
+export const users = []
+const users = [
+    {
+        id: 1,
+        name: "Juan",
+        email:"asasfa@gmail.com",
+        password: "1234",
+        role: "admin"    
+    },
+    {
+        id: 2,
+        name: "Pedro",
+        email:"3325egdas@gmail.com",
+        password: "1234",
+        role: "user"    
+    }
+];
+
+export const alumnos = [
+    {
+        id: 1,
+        name: "Roberto",
+        age:"22",
+        course:"science" 
+    },
+    {
+        id: 2,
+        name: "Pablo",
+        age:"20",
+        course:"math" 
+    }
+];
