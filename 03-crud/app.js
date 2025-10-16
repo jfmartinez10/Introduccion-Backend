159490d87083306db2a95c81164f02bf82f2c4a5
jfmartinez10 <jfmartinez383@gmail.com>
2025-10-16 14:23:08 +0200

    Feat: 03-crud

---
+++ 03-crud/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+
+import logger from "./middlewares/loggers.js";
+
+import mainRouter from "./routes/index.js";
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+//Middleware para entender JSON
+app.use(express.json());
+
+//Middleware de loggeo
+app.use(logger);
+
+//Endpoint raiz
+app.get("/", (req, res) => { //request y response
+    //Enviamos desde tipo json
+    res.send({
+        mensaje: "Hola mundo"
+    })
+})
+
+//cargar rutas
+app.use("/api", mainRouter);
+
+//Escuchar en el puerto 3000
+app.listen(PORT, () => {
+    console.log("Servidor escuchando el puerto " + PORT);
+})
