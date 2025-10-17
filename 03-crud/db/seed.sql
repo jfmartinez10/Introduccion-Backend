b5981b71e84507047fb1d0f0d3922b7538c84dd0
jfmartinez10 <jfmartinez383@gmail.com>
2025-10-17 11:18:28 +0200

    Implementación db

---
+++ 03-crud/db/seed.sql
@@ -0,0 +1,21 @@
+
+CREATE DATABASE granja_abejas IF NOT EXISTS;
+
+\c granja_abejas;
+
+DROP TABLE IF EXISTS clientes;
+CREATE TABLE clientes (
+	id_cliente SERIAL PRIMARY KEY,
+	nombre VARCHAR(100) NOT NULL,
+	telefono VARCHAR(20),
+	UBICACION TEXT
+);
+
+DROP TABLE IF EXISTS pedidos;
+CREATE TABLE pedidos (
+	id_pedidos SERIAL PRIMARY KEY,
+	id_cliente INT NOT NULL REFERENCES clientes(id_cliente) ON DELETE CASCADE,
+	fecha_pedido DATE NOT NULL DEFAULT CURRENT_DATE,
+	descripcion TEXT,
+	cantidad INT CHECK (cantidad > 0)
+);
