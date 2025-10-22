cdb85f0f919aa8310db3bc3ae5a48ff7ab0e22e0
jfmartinez10 <jfmartinez383@gmail.com>
2025-10-22 20:59:02 +0200

    Feat: loggers

--- 03-crud/middlewares/loggers.js
+++ 03-crud/middlewares/loggers.js
@@ -1,9 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+
 //Middleware para loggear peticiones
 //Export permite importarlo en otros ficheros
 
 const logger = (req, res, next) => {
-    console.log(`[${new Date().toISOString()}] ${req.method} - ${req.socket.remoteAddress}`); //Se escribe en este formato para contatenar más rápido
-    next();
-};
+    const string = `[${new Date().toISOString()}] ${req.method} - ${req.url} - ${req.ip}`
 
-export default logger;
+    console.log(string);
+
+    fs.appendFile('./logs/request.log',
+        string + '\n',
+        (error) => {
+            if(error) {
+                console.log( error);
+            }
+        }
+    );
+    next();
+}
