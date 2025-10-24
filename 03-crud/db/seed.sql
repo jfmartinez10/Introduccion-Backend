b5bb364c05a651fd40ec6fb396d4c1207c329a0f
jfmartinez10 <jfmartinez383@gmail.com>
2025-10-24 10:31:50 +0200

    Fix: Error corregido en la tabla logs

--- 03-crud/db/seed.sql
+++ 03-crud/db/seed.sql
@@ -20,44 +20,10 @@ CREATE TABLE pedidos (
 );
 
 DROP TABLE IF EXISTS logs;
-import fs from 'fs';
-import path from 'path';
-import { newLogService } from '../services/logs.service.js'; 
-
-
-export const logger = (req, res, next) => {
-    const fecha_actual = new Date(); 
-    
-
-    const metodo = req.method;
-    const url = req.url;
-    const ip = req.socket.remoteAddress;
-
-    const string = `[${fecha_actual.toISOString()}] ${metodo} - ${url} - ${ip} `;
-    console.log(string);
-    
-    const date = fecha_actual.toISOString().slice(0, 10);
-    let logPath = path.resolve("./logs/");
-
-    // Guarda el log en la base de datos
-    newLogService(fecha_actual.toISOString(), metodo, ip, url)
-        .catch(error => console.error("Error al guardar log en DB:", error.message));
-    
-    // Asegura que el directorio exista (solo si no existe)
-    if (!fs.existsSync(logPath)) {
-        fs.mkdirSync(logPath);
-    }
-
-    logPath = path.join(logPath, date + '.log');
-
-    // Se guarda el log en el archivo
-    fs.appendFile(logPath, 
-        string + '\n',
-        (error) => {
-            if (error) {
-                console.error("Error al guardar log en archivo:", error);
-            }
-        }
-    );
-    next();
-};
+CREATE TABLE logs (
+    id_log SERIAL PRIMARY KEY ON DELETE CASCADE,
+    fecha_log TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+    metodo VARCHAR(10) NOT NULL,
+    ip VARCHAR(45) NOT NULL,
+    url VARCHAR(255) NOT NULL
+);
