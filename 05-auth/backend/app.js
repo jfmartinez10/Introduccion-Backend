bc89f813bb9dc404576ee3835270224c250d9887
jfmartinez10 <jfmartinez383@gmail.com>
2025-11-21 08:58:12 +0100

    Feat: Introduccion de la nueva carpeta 05-auth. Creamos un app.js sencillo, importando envLoader y logger.js, archivos que hemos creado en la carpeta utils

---
+++ 05-auth/backend/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+import ENV from './utils/envLoader.js';
+import logger from './utils/logger.js';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(logger);
+
+app.get('/', (req, res) => {
+  res.send('Hello, Auth!');
+});
+
+
+app.listen(ENV.PORT, () => {
+    console.log('Server is running on port: ' + ENV.PORT);
+});
